176d557256e8976bd26758c7729b1447caea64bf
jpalencia77 <jpalencia@ucol.mx>
2023-01-07 09:53:54 -0600

    filter searchBar 1

--- src/App.js
+++ src/App.js
@@ -16,30 +16,52 @@ class App extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-     .then(response => response.json())
-     .then((users) => this.setState(() => {
-      return {monsters: users}
-     },
-     
-     () => console.log(this.state)
-     ))
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+
+        () => console.log(this.state)
+      ))
   }
 
 
   render() {
     return (
       <div className="App">
+
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value)
+
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters }
+            })
+
+          }}
+
+        />
+
+
         {
           this.state.monsters.map((monster) => {
             return (
 
               <div key={monster.id}>
 
-                <h1  >{monster.name}</h1>
+                <h1>{monster.name}</h1>
               </div>
-            
+
             )
           })
         }
